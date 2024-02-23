54ef9ed9501892094a19602e492e7d285d58fa27
IvanJanssens <IvanJanssens@users.noreply.github.com>
2024-02-23 13:44:12 +0000

    chore

--- conventional_pre_commit/hook.py
+++ conventional_pre_commit/hook.py
@@ -19,7 +19,7 @@ def main(argv=[]):
         prog="conventional-pre-commit", description="Check a git commit message for Conventional Commits formatting."
     )
     parser.add_argument("types", type=str, nargs="*", default=format.DEFAULT_TYPES, help="Optional list of types to support")
-    parser.add_argument("--prefix", dest="prefix", type=str, nargs="*", help="Optional list of prefixes")
+    parser.add_argument("--prefix", type=str, nargs="*", help="Optional list of prefixes")
     parser.add_argument("input", type=str, help="A file containing a git commit message")
     parser.add_argument(
         "--force-scope", action="store_false", default=True, dest="optional_scope", help="Force commit to have scope defined."
